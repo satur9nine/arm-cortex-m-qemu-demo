4bc002854dbe6f1e45c8ee4f9a6a27776e6fcb58
Jacob Abrams <satur9nine@gmail.com>
2024-08-03 13:16:52 -0700

    Can debug

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-arm -M lm3s6965evb -kernel Debug/RTOSDemo.elf -gdb tcp::1234,ipv4 -S
+qemu-system-arm -M lm3s6965evb -kernel Debug/arm-cortex-qemu-demo.elf -gdb tcp::1234,ipv4 -S
